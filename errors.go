775b79cdb9bae0b8fc2c956794a86002a5638bf5
Sasha Varlamov <sasha@sashavarlamov.com>
2015-08-24 15:14:35 +0800

    Add message field to the *Verbose functions

--- errors.go
+++ errors.go
@@ -6,22 +6,22 @@ import (
 
 func ReportError(err error, message string) {
     m := make(map[string]string)
-    m["message"] = message
-    ReportCriticalVerbose(err, m)
+    ReportCriticalVerbose(err, message, m)
 }
 
 func ReportCritical(err error, message string) {
     m := make(map[string]string)
-    m["message"] = message
-    ReportCriticalVerbose(err, m)
+    ReportCriticalVerbose(err, message, m)
 }
 
-func ReportErrorVerbose(err error, fields map[string]string) {
+func ReportErrorVerbose(err error, message string, fields map[string]string) {
+    fields["message"] = message
     rollbarFields := mapToRollbarFields(fields)
     rollbar.Error("error", err, rollbarFields...)
 }
 
-func ReportCriticalVerbose(err error, fields map[string]string) {
+func ReportCriticalVerbose(err error, message string, fields map[string]string) {
+    fields["message"] = message
     rollbarFields := mapToRollbarFields(fields)
     rollbar.Error("critical", err, rollbarFields...)
 }
